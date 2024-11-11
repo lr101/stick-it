6d210b484c16f09f6269221a33b5a7fae766d2fb
Lukas R. <Lukasr101@proton.me>
2024-11-11 13:07:05 +0800

    Remove fetching of pins before starting map

---
+++ lib/features/map_home/data/map_state.freezed.dart
@@ -0,0 +1,150 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'map_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$MapState {
+  List<Marker> get markers => throw _privateConstructorUsedError;
+
+  /// Create a copy of MapState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $MapStateCopyWith<MapState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MapStateCopyWith<$Res> {
+  factory $MapStateCopyWith(MapState value, $Res Function(MapState) then) =
+      _$MapStateCopyWithImpl<$Res, MapState>;
+  @useResult
+  $Res call({List<Marker> markers});
+}
+
+/// @nodoc
+class _$MapStateCopyWithImpl<$Res, $Val extends MapState>
+    implements $MapStateCopyWith<$Res> {
+  _$MapStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of MapState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? markers = null,
+  }) {
+    return _then(_value.copyWith(
+      markers: null == markers
+          ? _value.markers
+          : markers // ignore: cast_nullable_to_non_nullable
+              as List<Marker>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$MapStateImplCopyWith<$Res>
+    implements $MapStateCopyWith<$Res> {
+  factory _$$MapStateImplCopyWith(
+          _$MapStateImpl value, $Res Function(_$MapStateImpl) then) =
+      __$$MapStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({List<Marker> markers});
+}
+
+/// @nodoc
+class __$$MapStateImplCopyWithImpl<$Res>
+    extends _$MapStateCopyWithImpl<$Res, _$MapStateImpl>
+    implements _$$MapStateImplCopyWith<$Res> {
+  __$$MapStateImplCopyWithImpl(
+      _$MapStateImpl _value, $Res Function(_$MapStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of MapState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? markers = null,
+  }) {
+    return _then(_$MapStateImpl(
+      markers: null == markers
+          ? _value._markers
+          : markers // ignore: cast_nullable_to_non_nullable
+              as List<Marker>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$MapStateImpl implements _MapState {
+  const _$MapStateImpl({required final List<Marker> markers})
+      : _markers = markers;
+
+  final List<Marker> _markers;
+  @override
+  List<Marker> get markers {
+    if (_markers is EqualUnmodifiableListView) return _markers;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_markers);
+  }
+
+  @override
+  String toString() {
+    return 'MapState(markers: $markers)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$MapStateImpl &&
+            const DeepCollectionEquality().equals(other._markers, _markers));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(_markers));
+
+  /// Create a copy of MapState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MapStateImplCopyWith<_$MapStateImpl> get copyWith =>
+      __$$MapStateImplCopyWithImpl<_$MapStateImpl>(this, _$identity);
+}
+
+abstract class _MapState implements MapState {
+  const factory _MapState({required final List<Marker> markers}) =
+      _$MapStateImpl;
+
+  @override
+  List<Marker> get markers;
+
+  /// Create a copy of MapState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$MapStateImplCopyWith<_$MapStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
