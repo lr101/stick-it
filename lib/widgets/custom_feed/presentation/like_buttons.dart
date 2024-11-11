a684aa6f681b06b26b5ed2d11a0b04d63559aa0b
Lukas R. <Lukasr101@proton.me>
2024-11-11 09:58:34 +0800

    Animate  liking image on double tab

--- lib/widgets/custom_feed/presentation/like_buttons.dart
+++ lib/widgets/custom_feed/presentation/like_buttons.dart
@@ -4,11 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:like_button/like_button.dart';
 import 'package:openapi/api.dart';
 
 import '../../../data/service/global_data_service.dart';
 import '../data/like_service.dart';
+import 'like_button_animated.dart';
 
 class LikeButtons extends ConsumerWidget {
 
@@ -22,8 +22,10 @@ class LikeButtons extends ConsumerWidget {
     final userId = ref.watch(globalDataServiceProvider).userId!;
     return Row(
           children: [
-            LikeButton(
+            LikeButtonAnimated(
+              isLikedProvider: pinLikeProvider(pinId).select((e) => e.valueOrNull?.likedByUser ?? false),
               isLiked: pinLike.valueOrNull?.likedByUser ?? false,
+              postFrameCallback: (state) => addLikeState(state, pinLike.valueOrNull?.likedByUser ?? false),
               likeBuilder: (isLiked) {
                 return Icon(
                   Icons.favorite,
@@ -46,8 +48,10 @@ class LikeButtons extends ConsumerWidget {
               },
             ),
             SizedBox(width: 10),
-            LikeButton(
+            LikeButtonAnimated(
+              isLikedProvider: pinLikeProvider(pinId).select((e) => e.valueOrNull?.likedLocationByUser ?? false),
               isLiked: pinLike.valueOrNull?.likedLocationByUser ?? false,
+              postFrameCallback: (state) => addLikeState(state, pinLike.valueOrNull?.likedLocationByUser ?? false),
               likeBuilder: (isLiked) {
                 return Icon(
                   CupertinoIcons.location_solid,
@@ -57,7 +61,6 @@ class LikeButtons extends ConsumerWidget {
               },
               likeCount: pinLike.valueOrNull?.likeLocationCount ?? 0,
               onTap: (isLiked) async {
-
                 try {
                   if (isLiked) {
                     await ref.read(likeServiceProvider.notifier).addLike(pinId, CreateLikeDto(userId: userId, likeLocation: false));
@@ -71,8 +74,10 @@ class LikeButtons extends ConsumerWidget {
               },
             ),
             SizedBox(width: 10),
-            LikeButton(
+            LikeButtonAnimated(
+              isLikedProvider: pinLikeProvider(pinId).select((e) => e.valueOrNull?.likedPhotographyByUser ?? false),
               isLiked: pinLike.valueOrNull?.likedPhotographyByUser ?? false,
+              postFrameCallback: (state) => addLikeState(state, pinLike.valueOrNull?.likedPhotographyByUser ?? false),
               likeBuilder: (isLiked) {
                 return Icon(
                   Icons.photo_camera,
@@ -95,8 +100,10 @@ class LikeButtons extends ConsumerWidget {
               },
             ),
             SizedBox(width: 10),
-            LikeButton(
+            LikeButtonAnimated(
+              isLikedProvider: pinLikeProvider(pinId).select((e) => e.valueOrNull?.likedArtByUser ?? false),
               isLiked: pinLike.valueOrNull?.likedArtByUser ?? false,
+              postFrameCallback: (state) => addLikeState(state, pinLike.valueOrNull?.likedArtByUser ?? false),
               likeBuilder: (isLiked) {
                 return Icon(
                   Icons.palette,
@@ -106,7 +113,6 @@ class LikeButtons extends ConsumerWidget {
               },
               likeCount: pinLike.valueOrNull?.likeArtCount ?? 0,
               onTap: (isLiked) async {
-                final userId = ref.watch(globalDataServiceProvider).userId!;
                 try {
                   if (isLiked) {
                     await ref.read(likeServiceProvider.notifier).addLike(pinId, CreateLikeDto(userId: userId, likeArt: false));
@@ -121,6 +127,13 @@ class LikeButtons extends ConsumerWidget {
             )
           ],
         );
+  }
+
+  void addLikeState(LikeButtonAnimatedState state, bool isLiked) {
+    if (isLiked) {
+      state.controller!.reset();
+      state.controller!.forward();
+    }
   }
 
 }
