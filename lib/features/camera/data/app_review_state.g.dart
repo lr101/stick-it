497391107b35c8973fa614d6957d10a195d21b9c
Lukas Reim <lukasr101@proton.me>
2024-12-15 10:11:38 +0800

    22 add rate map app (#24)
    * Add review page
    
    * Update review page

---
+++ lib/features/camera/data/app_review_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_review_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appReviewStateHash() => r'7b01b802f5f4db2318a5f73a8cf26be9d110869e';
+
+/// See also [AppReviewState].
+@ProviderFor(AppReviewState)
+final appReviewStateProvider =
+    AutoDisposeNotifierProvider<AppReviewState, bool>.internal(
+  AppReviewState.new,
+  name: r'appReviewStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appReviewStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppReviewState = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
